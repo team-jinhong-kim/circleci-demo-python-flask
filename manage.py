c2d9418a7bc068eac3ac043aff25b8a553e5433a
team-jinhong-kim <59462921+team-jinhong-kim@users.noreply.github.com>
2022-03-13 18:02:14 +0900

    remove useless comments

--- manage.py
+++ manage.py
@@ -13,21 +13,18 @@ if os.path.exists('.env'):
         if len(var) == 2:
             os.environ[var[0]] = var[1]
 
-from app import create_app, db
-from app.models import User, Follow, Role, Permission, Post, Comment
+from app import create_app
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
-migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Follow=Follow, Role=Role,
+    return dict(app=app, User=User, Follow=Follow, Role=Role,
                 Permission=Permission, Post=Post, Comment=Comment)
 manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
 
 
 @manager.command
@@ -66,17 +63,7 @@ def profile(length=25, profile_dir=None):
 @manager.command
 def deploy():
     """Run deployment tasks."""
-    from flask_migrate import upgrade
-    from app.models import Role, User
-
-    # migrate database to latest revision
-    upgrade()
-
-    # create user roles
-    Role.insert_roles()
-
-    # create self-follows for all users
-    User.add_self_follows()
+    print("Run deployment tasks.")
 
 
 if __name__ == '__main__':
