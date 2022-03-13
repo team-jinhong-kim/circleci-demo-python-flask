f890adec5d549cfab25feea32fdb3f154b772a59
team-jinhong-kim <59462921+team-jinhong-kim@users.noreply.github.com>
2022-03-13 18:49:03 +0900

    modify app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,10 @@
-from flask import redirect, url_for
+from flask import redirect, url_for, request
 from . import main
 
 @main.route('/', methods=['GET'])
 def index():
     return redirect(url_for('.index'))
+
+@main.route('/shutdown')
+def server_shutdown():
+    shutdown = request.environ.get('werkzeug.server.shutdown')
