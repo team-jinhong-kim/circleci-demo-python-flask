7affc06181f0a3833d2f077d3f653c7a3a17b4ea
team-jinhong-kim <59462921+team-jinhong-kim@users.noreply.github.com>
2022-03-13 17:57:59 +0900

    remove useless parts

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -3,13 +3,12 @@ import threading
 import time
 import unittest
 from selenium import webdriver
-from app import create_app, db
-from app.models import Role, User, Post
+from app import create_app
 
 
 class SeleniumTestCase(unittest.TestCase):
     client = None
-    
+
     @classmethod
     def setUpClass(cls):
         # start Chrome
@@ -30,25 +29,11 @@ class SeleniumTestCase(unittest.TestCase):
             logger = logging.getLogger('werkzeug')
             logger.setLevel("ERROR")
 
-            # create the database and populate with some fake data
-            db.create_all()
-            Role.insert_roles()
-            User.generate_fake(10)
-            Post.generate_fake(10)
-
-            # add an administrator user
-            admin_role = Role.query.filter_by(permissions=0xff).first()
-            admin = User(email='john@example.com',
-                         username='john', password='cat',
-                         role=admin_role, confirmed=True)
-            db.session.add(admin)
-            db.session.commit()
-
             # start the Flask server in a thread
             threading.Thread(target=cls.app.run).start()
 
             # give the server a second to ensure it is up
-            time.sleep(1) 
+            time.sleep(1)
 
     @classmethod
     def tearDownClass(cls):
@@ -56,10 +41,6 @@ class SeleniumTestCase(unittest.TestCase):
             # stop the flask server and the browser
             cls.client.get('http://localhost:5000/shutdown')
             cls.client.close()
-
-            # destroy database
-            db.drop_all()
-            db.session.remove()
 
             # remove application context
             cls.app_context.pop()
@@ -70,24 +51,8 @@ class SeleniumTestCase(unittest.TestCase):
 
     def tearDown(self):
         pass
-    
+
     def test_admin_home_page(self):
         # navigate to home page
         self.client.get('http://localhost:5000/')
-        self.assertTrue(re.search('Hello,\s+Stranger!',
-                                  self.client.page_source))
-
-        # navigate to login page
-        self.client.find_element_by_link_text('Log In').click()
-        self.assertTrue('<h1>Login</h1>' in self.client.page_source)
-
-        # login
-        self.client.find_element_by_name('email').\
-            send_keys('john@example.com')
-        self.client.find_element_by_name('password').send_keys('cat')
-        self.client.find_element_by_name('submit').click()
-        self.assertTrue(re.search('Hello,\s+john!', self.client.page_source))
-
-        # navigate to the user's profile page
-        self.client.find_element_by_link_text('Profile').click()
-        self.assertTrue('<h1>john</h1>' in self.client.page_source)
+        self.assertTrue(driver.getPageSource().contains('Random'));
