87481c010db7f0805a78696105477513ef80f246
team-jinhong-kim <59462921+team-jinhong-kim@users.noreply.github.com>
2022-03-13 18:32:50 +0900

    modify manage.py

--- manage.py
+++ manage.py
@@ -37,7 +37,8 @@ def test(coverage=False):
     import unittest
     # import xmlrunner
     tests = unittest.TestLoader().discover('tests')
-    tests.run()
+    runner = unittest.TextTestRunner()
+    runner.run(tests())
     # run tests with unittest-xml-reporting and output to $CIRCLE_TEST_REPORTS on CircleCI or test-reports locally
     # xmlrunner.XMLTestRunner(output=os.environ.get('CIRCLE_TEST_REPORTS','test-reports')).run(tests)
     if COV:
