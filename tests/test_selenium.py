e0582d6c3904c8b8469e00092f55c2dd565866b4
team-jinhong-kim <59462921+team-jinhong-kim@users.noreply.github.com>
2022-03-15 02:24:49 +0900

    modify testing

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -52,21 +52,21 @@ class SeleniumTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_home_page(self):
+    def test_home_page(cls):
         # navigate to home page
-        self.client.get('http://localhost:5000/')
-        self.assertTrue(re.search('Generator', self.client.page_source))
+        cls.client.get('http://localhost:5000/')
+        cls.assertTrue(re.search('Generator', cls.client.page_source))
 
-    def test_button(self):
+    def test_button(cls):
         # finding the button using ID
-        self.client.get('http://localhost:5000/')
-        button = self.client.find_element_by_id('create-random-string')
+        cls.client.get('http://localhost:5000/')
+        button = cls.client.find_element_by_id('create-random-string')
 
         # clicking on the button
         button.click()
 
         # Check randomfield
-        element = self.client.find_element_by_id('randomfield')
+        element = cls.client.find_element_by_id('randomfield')
         text = element.text
 
         # clicking button again then compare to confirm they are not equal
