b4842dcc040ef98e84c4c881929dd03390099fbf
team-jinhong-kim <59462921+team-jinhong-kim@users.noreply.github.com>
2022-03-15 00:46:26 +0900

    modify config.py a bit

--- config.py
+++ config.py
@@ -13,20 +13,14 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'postgresql://localhost/circulate'
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'postgresql://localhost/circulate'
     WTF_CSRF_ENABLED = False
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://localhost/circulate'
 
     @classmethod
     def init_app(cls, app):
